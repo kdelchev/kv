626365271764f40da30b546374e51a6d4e3a6aa4
Konstantin Delchev <kdelchev@gmail.com>
2016-10-06 19:16:16 +0300

    Add delete function for bucket

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -19,4 +19,13 @@ defmodule KV.Bucket do
   def update(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
   end
+
+  @doc """
+  Deletes `key` from bucket.
+
+  Returns the current value of `key`, if `key` sxists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
