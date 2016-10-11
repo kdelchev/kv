30a34bffb298daa7f16dca1a93a3d00a1900d0d5
Konstantin Delchev <kdelchev@gmail.com>
2016-10-11 13:27:25 +0300

    Add handlers to pass test

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -16,7 +16,7 @@ defmodule KV.RegistryTest do
     assert KV.Bucket.get(bucket, "milk") == 1
   end
 
-  test "removes bucket on exut", %{registry: registry} do
+  test "removes buckets on exit", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
